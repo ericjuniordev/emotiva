9c0d5dc3420d40e7da5d2ce188f575f3bc47152f
wataredev <158059889+wataredev@users.noreply.github.com>
2024-06-08 18:36:13 -0300

    feat: projeto heverton

---
+++ alunos/heverton-victor/heverton.js
@@ -0,0 +1,9 @@
+let textArea = document.querySelector("textArea");
+
+function um() {
+    document.getElementById("textarea").innerHTML = "1";
+}
+
+function dois() {
+    document.getElementById("textarea").innerHTML = "2";
+}
